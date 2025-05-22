7d40ec5066acb44a2211db92b61d435944923f35
TimelyCo <anmolraturi25@gmail.com>
2025-05-22 15:24:26 +0530

    Added JWT-based authentication and role-based access control

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const { registerUser, loginUser } = require('../controllers/authController');
-
+const { protect, adminOnly } = require('../middleware/authMiddleWare');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.get('/test-admin', protect, adminOnly, (req, res) => {
+  res.send(`Hello Admin ${req.user.name}`);
+});
 
 module.exports = router;
