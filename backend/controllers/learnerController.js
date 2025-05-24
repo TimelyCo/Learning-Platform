7d81ea60cc0f69a9e30fc4548a01b0e8c1c13219
TimelyCo <anmolraturi25@gmail.com>
2025-05-24 12:56:48 +0530

    added getAllCourses component in learnerController

--- backend/controllers/learnerController.js
+++ backend/controllers/learnerController.js
@@ -98,3 +98,13 @@ exports.saveChapterProgress = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.status(200).json(courses);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
--- backend/routes/learnerRoutes.js
+++ backend/routes/learnerRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { protect, learnerOnly } = require('../middleware/authMiddleWare');
 const { getCourseContent } = require('../controllers/learnerController');
 const { saveChapterProgress } = require('../controllers/learnerController');
+const { getAllCourses } = require('../controllers/learnerController');
 
 router.get('/course/:id', protect, learnerOnly, getCourseContent);
 router.post('/progress', protect, learnerOnly, saveChapterProgress);
+router.get('/courses', protect, learnerOnly, getAllCourses);
 
 module.exports = router;
